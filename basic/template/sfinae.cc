040bd96d0099b0a25f7e9cb67f4b1ad4c77bee2a
zhaorui <czhaor@gmail.com>
2023-11-04 21:57:32 +0800

    update.

--- basic/template/sfinae.cc
+++ basic/template/sfinae.cc
@@ -1,13 +1,15 @@
-#include <type_traits>
 #include <concepts>
+#include <type_traits>
 
-// NOTE: SFINAE 是指模板替换失败时，并不会报错。但是这个特性能够应用到模板元编程中。
+// NOTE: SFINAE
+// 是指模板替换失败时，并不会报错。但是这个特性能够应用到模板元编程中。
 
 // helper classes
 class Null {};
 template <typename T> struct dump;
 
-// ENUM HACK: old fasion before c++11
+// N ways to write a c++ traits 
+// 1. enum hack: old fasion before c++11
 template <typename T> struct enum_is_pointer {
   enum { value = false };
   typedef Null type;
@@ -18,7 +20,7 @@ template <typename T> struct enum_is_pointer<T *> {
   typedef T *type;
 };
 
-// static intialization: old fasion in c++11
+// 2. static intialization: old fasion in c++11
 template <typename T> struct static_is_pointer {
   const static bool value = false;
   typedef Null type;
@@ -28,7 +30,7 @@ template <typename T> struct static_is_pointer<T *> {
   typedef T *type;
 };
 
-// constexpr: best practise
+// 3. constexpr
 template <typename T> struct constexpr_is_pointer {
   static constexpr bool value = false;
   typedef Null type;
@@ -38,48 +40,44 @@ template <typename T> struct constexpr_is_pointer<T *> {
   typedef T *type;
 };
 
-// ENABLE_IF
-template <typename T, class Enable = void> struct enable_if_is_pointer {
-  static constexpr bool value = false;
+// 4. std::enable_if
+template <typename T, class Enable = void>
+struct enable_if_is_pointer : std::false_type {
   typedef Null type;
 };
 template <typename T>
-struct enable_if_is_pointer<T, typename std::enable_if<std::is_pointer_v<T>>::type> {
-  static constexpr bool value = true;
+struct enable_if_is_pointer<T,
+                            typename std::enable_if<std::is_pointer_v<T>>::type>
+    : std::true_type {
   typedef T type;
 };
 
-// if constexpr
-template<typename T>
-constexpr bool if_constexpr_is_pointer() {
+// 5. if constexpr
+template <typename T> constexpr bool if_constexpr_is_pointer() {
   if constexpr (std::is_pointer_v<T>) {
     return true;
   }
   return false;
 }
 
-// VOID_T
-template<typename T, class Enable = void>
-struct void_t_is_pointer {
-  static constexpr bool value = false;
+// 6. std::void_t
+template <typename T, class Enable = void>
+struct void_t_is_pointer : std::false_type {
   typedef Null type;
 };
-template<typename T>
-struct void_t_is_pointer<T, std::void_t<decltype(*std::declval<T>())>> {
-  static constexpr bool value = true;
+template <typename T>
+struct void_t_is_pointer<T, std::void_t<decltype(*std::declval<T>())>>
+    : std::true_type {
   typedef T type;
 };
 
-// concepts && requires
-template<typename T>
-struct require_pointer {
-  static constexpr bool value = false;
+// 7. concepts && requires
+template <typename T> struct require_pointer : std::false_type {
   typedef Null type;
 };
-template<typename T>
-requires std::is_pointer_v<T>
-struct require_pointer<T> {
-  static constexpr bool value = true;
+template <typename T>
+  requires std::is_pointer_v<T>
+struct require_pointer<T> : std::true_type {
   typedef T type;
 };
 
@@ -114,7 +112,7 @@ int main() {
   static_assert(std::is_same_v<void_t_is_pointer<int *>::type, int *>);
 
   static_assert(require_pointer<int>::value == false);
-  static_assert(require_pointer<int*>::value == true);
+  static_assert(require_pointer<int *>::value == true);
   static_assert(std::is_same_v<require_pointer<int>::type, Null>);
   static_assert(std::is_same_v<require_pointer<int *>::type, int *>);
 }
