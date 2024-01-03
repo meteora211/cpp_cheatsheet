100c74f4ef7666c3420b05a75e37e7104392a71d
zhaorui chen <czhaor@gmail.com>
2024-01-03 22:31:29 +0800

    update inline.

--- std/cpp17/inline.cc
+++ std/cpp17/inline.cc
@@ -8,23 +8,32 @@
 
 // ref: https://zhuanlan.zhihu.com/p/394975612
 
-// method 1
+// method 1: define static outside
 struct MyClass1 {
   static std::string msg;
 };
 
-std::string MyClass1::msg {"OK"};
+std::string MyClass1::msg {"1.OK"};
 
-// method 2
+// method 2: static local variable
 struct MyClass2 {
   static std::string getMsg() {
-    static std::string msg {"OK"};
+    static std::string msg {"2.OK"};
     return msg;
   }
 };
 
+// method 3: variable template (since c++14)
+template<typename T = std::string> T VTmsg{"VT.OK"};
+
+// C++17: inline static
+struct MyClass3 {
+  inline static std::string msg{"3.OK"};
+};
 
 int main() {
   std::cout << MyClass1::msg << std::endl;
   std::cout << MyClass2::getMsg() << std::endl;
+  std::cout << VTmsg<> << std::endl;
+  std::cout << MyClass3::msg << std::endl;
 }
