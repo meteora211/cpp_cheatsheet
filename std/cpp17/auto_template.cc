fe0a6ccb38a65ca8ebdf1434dc956387dbbee115
zhaorui chen <czhaor@gmail.com>
2024-01-12 22:26:46 +0800

    add auto template

---
+++ std/cpp17/auto_template.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+
+template <auto N> class S {};
+
+// template<auto... VS>
+// struct HeteroList {};
+
+// template<auto V1, decltype(V1)... VS>
+// struct HomoList {};
+
+template <auto... VS> class HeteroValueList {};
+template <auto V1, decltype(V1)... VS> class HomoValueList {};
+// XXX: same with above and vals4 also not fail in below version.
+// template <int... VS> class HomoValueList {};
+
+int main() {
+  S<42> s1;  // N -> int
+  S<'a'> s2; // N -> char
+  // s<2.5> s3; // error, double can not be template param
+
+  HeteroValueList<1, 2, 3> vals1;      // ok
+  HeteroValueList<1, 'a', true> vals2; // ok
+
+  HomoValueList<1, 2, 3> vals3;      // ok
+  // XXX: vals4 has no compile error, not sure why
+  HomoValueList<1, 'a', true> vals4; // error
+}
