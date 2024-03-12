99af0cacb6e0f57e23470fdd32bf9df06a2fe7a4
zhaorui chen <czhaor@gmail.com>
2024-03-13 00:01:06 +0800

    update forward.

--- stl/move_forward/forward_test.cc
+++ stl/move_forward/forward_test.cc
@@ -13,21 +13,28 @@ void print(T&& t) {
 
 template<typename T>
 void test_print_common(T&& t) {
-  print<T>(t);                  // T=int -> lref, T=int& -> lref, T=int&& -> lref
-  print(std::forward<T>(t));    // T=int -> lref, T=int& -> lref, T=int&& -> rref
-  print(std::forward<T&>(t));   // T=int -> lref, T=int& -> lref, T=int&& -> lref
-  print(std::forward<T&&>(t));  // T=int -> lref, T=int& -> lref, T=int&& -> rref
-  print(std::move(t));          // T=int -> rref, T=int& -> rref, T=int&& -> rref
+  print(t);                     // t=int& -> lref, t=int& && -> lref, t=int&& -> lref
+  print(std::forward<T>(t));    // t=int& -> lref, t=int& && -> lref, t=int&& -> rref
+  print(std::forward<T&>(t));   // t=int& -> lref, t=int& && -> lref, t=int&& -> lref
+  print(std::forward<T&&>(t));  // t=int& -> lref, t=int& && -> lref, t=int&& -> rref
+  print(std::move(t));          // t=int& -> rref, t=int& && -> rref, t=int&& -> rref
 }
 
 template<typename T>
 void test_print_value(T t) {
-  print<T>(t);                  // T=int -> lref, T=int& -> lref, T=int&& -> lref
-  print(std::forward<T>(t));    // T=int -> rref, T=int& -> rref, T=int&& -> rref
-  print(std::forward<T&>(t));   // T=int -> lref, T=int& -> lref, T=int&& -> lref
-  print(std::forward<T&&>(t));  // T=int -> rref, T=int& -> rref, T=int&& -> rref
-  print(std::move(t));          // T=int -> rref, T=int& -> rref, T=int&& -> rref
+  print(t);                     // t=int -> lref, t=int& -> lref, t=int&& -> lref
+  print(std::forward<T>(t));    // t=int -> rref, t=int& -> rref, t=int&& -> rref
+  print(std::forward<T&>(t));   // t=int -> lref, t=int& -> lref, t=int&& -> lref
+  print(std::forward<T&&>(t));  // t=int -> rref, t=int& -> rref, t=int&& -> rref
+  print(std::move(t));          // t=int -> rref, t=int& -> rref, t=int&& -> rref
 }
+
+template<typename T>
+struct TestPrint {
+  void call(T t) {
+    print<T>(std::forward<T>(t));    // t=int -> rref, t=int& -> lref, t=int&& -> rref
+  }
+};
 
 int main() {
   int x = 1;
@@ -40,4 +47,8 @@ int main() {
   test_print_value(x);
   test_print_value(xr);
   test_print_value(std::move(x));
+
+  TestPrint<decltype(x)>().call(x);
+  TestPrint<decltype(xr)>().call(xr);
+  TestPrint<decltype(std::move(x))>().call(std::move(x));
 }
